b3f6fe13cbb102cec7f7c0ec8b222e0d620dca32
abbey-glitch <abiodunonaolapi@gmail.com>
2026-06-29 14:58:06 +0100

    form process script added

---
+++ js/my.js
@@ -0,0 +1,29 @@
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const project = document.getElementById("project").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    const whatsappMessage =
+`*New Portfolio Contact*
+
+👤 Name: ${name}
+📧 Email: ${email}
+📞 Phone: ${phone}
+💼 Project: ${project}
+📌 Subject: ${subject}
+
+📝 Message:
+${message}`;
+
+    const phoneNumber = "08139357520";
+
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+
+    window.open(url, "_blank");
+});
